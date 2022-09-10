acec1abf7532b3de9f0db04db1e378f23320a3b9
Sujal <89023869+sujal2700@users.noreply.github.com>
2022-09-11 00:51:43 +0530

    update 3

--- src/components/movieinfo.js
+++ src/components/movieinfo.js
@@ -6,8 +6,11 @@ const Container=styled.div`
 display:flex;
 flex-direction:row;
 padding:20px 30px;
-justify-content:center;
+
 border-bottom: 1px solid lightgray;
+@media (max-width:600px){
+  display:inline-block;
+}
 `;
 const MovieName=styled.div`
   font-size:18px;
@@ -17,11 +20,19 @@ const MovieName=styled.div`
   white-space:nowrap;
   text-overflow:ellipsis;
   overflow:hidden;
+  @media (max-width:600px){
+    font-size:12px;
+  }
+  
 `;
 const CoverImage=styled.img`
-object-fit:cover;
+ 
 height:232px;
 padding:10px;
+@media (max-width:600px){
+  height:200px;
+  width:260px;
+}
 `;
 const Info=styled.span`
 font-size:18px;
@@ -29,28 +40,47 @@ font-weigth:600;
 color:black;
 margin:15px 0;
 white-space:nowrap;
-
 overflow:hidden;
+@media (max-width:600px){
+  font-size:12px;
+  font-weigth:500;
+  overflow-wrap:break-word;
+  
+}
 `;
 const MovieInfo=styled.div`
 font-size:16px;
 font-weight:500;
 color:black;
-overflow-wrap:break-word;
-overflow:hidden;
+@media (max-width:600px){
+  font-size:12px;
+  font-weigth:500;
+  overflow-wrap:break-word;
+}
 & span{
     opacity:0.5;
+    @media (max-width:600px){
+      font-size:10px;
+      font-weigth:500;
+      
+    }
+    
 }
+
 `;
 const Close=styled.button`
 font-size:10px;
 color:white;
 background:red;
 border-radius:0.4;
-height:18px;
-width:18px;
+border:none;
+width:60px;
 height:fit-content;
 cursor:pointer;
+font-weight:bold;
+&:hover{
+  border:1px-solid;
+}
 `;
 const MovieInfoComponent=(props)=>{
     const[movieInfo, setMovieInfo]=useState();
@@ -66,11 +96,11 @@ const MovieInfoComponent=(props)=>{
             <MovieInfo>IMDB Rating: <span>{movieInfo?.imdbRating}</span></MovieInfo>
             <MovieInfo>Language:<span>{movieInfo?.Language}</span></MovieInfo>
             <MovieInfo>Released:<span>{movieInfo?.Released}</span></MovieInfo>
-            <MovieInfo>Runtime: <span>{movieInfo?.Runtime}</span></MovieInfo>
+            <MovieInfo>Runtime: <span>{movieInfo?.Runtime}</span></MovieInfo> 
             <MovieInfo>Actors: <span>{movieInfo?.Actors}</span></MovieInfo>
             
         </Info>
-        <Close onClick={()=>props.onMovieSelect()}>X</Close>
+        <Close onClick={()=>props.onMovieSelect()}>Close</Close>
         </>):("Please wait .....")}
         </Container>
     );      
